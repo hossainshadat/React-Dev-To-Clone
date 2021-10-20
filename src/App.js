75d876b5c4bf0cdee68d8e6bd4ac538537a938c5
hossainshadat <hossainshadat501@gmail.com>
2021-10-20 20:20:25 +0600

    React App Devto Clone

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.scss";
+
+import Navigation from "./components/Navigation";
+import LeftSidebar from "./components/LeftSidebar";
+import Content from "./components/Content";
+import RightSidebar from "./components/RightSidebar";
+import { useState } from "react";
+
+function App() {
+  const [mobileMenu, setmobileMenu] = useState(false);
+  const toggle = () => {
+    setmobileMenu(!mobileMenu);
+  };
+
+  return (
+    <>
+      <Navigation openMenu={toggle} />
+
+      <main className="main-container">
+        <LeftSidebar burgerMenu={mobileMenu} closeMenu={toggle} />
+        <Content />
+        <RightSidebar />
+      </main>
+    </>
+  );
+}
+
+export default App;
